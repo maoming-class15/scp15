bcd51a00ce7c40707280be920b4a389faf8f85e2
Aflocat <2013390462@qq.com>
2024-10-16 18:36:44 +0800

    1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,29 +1,26 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/scp15/',
-  title: "scp15",
-  description: "收录……",
+  base: "/scp15/",
+  title: "15班收容所",
+  description: "嘻嘻",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: "Home", link: "/" },
+      { text: "面向写作者的教程", link: "/howtouse" },
     ],
 
     sidebar: [
+      { text: "主目录", items: [{ text: "麦镜隆", link: "/mai" }] },
       {
-        text: 'Examples',
+        text: "教程",
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: "面向写作者的教程", link: "/howtouse" },
+          { text: "面向开发与维护人员", link: "/api-examples" },
+        ],
+      },
     ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+  },
+});
